c92bff81e8c815806127879940549031595db8af
Gabriel Miranda <gabrielmirandaxl109@gmail.com>
2022-10-21 19:54:56 -0300

    rede-social

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import React,{useContext} from 'react'
+//pages
+import Home from "./pages/home/Home";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
+import Profile from './pages/profile/Profile'
+// context
+import { AuthContext } from "./context/AtuhContext";
+
+//components
+import Header from "./components/header/Header";
 
 function App() {
+
+  const {signed} = useContext(AuthContext)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+ 
+  <Router>
+     <Header/>
+    <Routes>
+      <Route path="/" element={ signed ? <Home/> : ''}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/register" element={<Register/>}/>
+      <Route path="/profile" element={ signed ? <Profile/> : ''} />
+    </Routes>
+
+  </Router>
+
+  </>
   );
 }
 
