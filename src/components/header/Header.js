2458bf1746ce4d4d212496a5a7adedf1a251b23a
Gabriel Miranda <gabrielmirandaxl109@gmail.com>
2022-11-21 18:56:50 -0300

    componentes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,83 +2,90 @@ import React,{useContext, useState, useEffect} from 'react'
 
 import "./Header.css"
 import { AuthContext } from '../../context/AtuhContext'
-import { Link, Navigate, NavLink } from 'react-router-dom'
+import { Link, NavLink, Navigate } from 'react-router-dom'
 import {BsSearch} from 'react-icons/bs'
 import axios from 'axios'
-import user from './pexels-justin-shaifer-1222271.jpg'
+import Avatar from '@mui/material/Avatar';
+
 
 const Header = () =>{
 
-const {signed, setToken} = useContext(AuthContext)
+const {signed, setToken, setPosts} = useContext(AuthContext)
 const [redirect, setRedirect] = useState(false)
 const [search, setSearch] = useState("")
-const [posts, setPosts] = useState([])
-const [image, setImage] = useState()
+const [image, setImage] = useState("")
+const [name, setName] = useState("")
+
 
 const handleLogout = () =>{
    localStorage.removeItem("token")
    setRedirect(true)
    setToken(null)
-   setRedirect(false)
+  
+ 
 }
 
 
 axios.interceptors.request.use(function(config){
   config.headers.authorization = `Bearer ${localStorage.getItem("token")}`
-  console.log(config)
   return config
 })
+
 
 useEffect(() =>{
   axios.get("http://localhost:8080/profile")
   .then((response) => {
-    console.log(response.data.profile)
-    setImage(response.data.profile)
-  
+    const {profileImage, name} = response.data
+    setImage(profileImage)
+    setName(name)
+  console.log(response)
    
   }) 
 }, [])
 
-const handleSubmitSearch = (e) =>{
-   e.preventDefault()
 
-   axios.get(`http://localhost:8080/search?q=${search}`)
-   .then((response) => setPosts(response.data))
-  
+const heandleSearch = (e) =>{
+  setSearch(e.target.value)
 }
-console.log(posts)
+
+  useEffect(() =>{
+    axios.get(`http://localhost:8080/search?q=${search}`)
+    .then((response) => setPosts(response.data))
+  }, [search])
+
+
+
+
   return(
     <>
-    { redirect && <Navigate to="/login"/> }
+    
+    {redirect ? <Navigate to="/auth"/> : ""}
     
     <header className='header'>
       <Link to="/" className='logo'>New<span className='logo2'>World</span></Link>
 
      {signed ?  <div className='search'>
-          <form method="post" onSubmit={handleSubmitSearch}>
+         
             <BsSearch className='icon-search'/>
               <input 
               type="text"
               name='q' 
               autoComplete='off'
-              onChange={(e) => setSearch(e.target.value)} 
+              onChange={(e) => heandleSearch(e)} 
               />
-          </form>
+       
+        
       </div> : ''}
 
       <nav className='nav'>
      
      <ul className='list-links'>
 
-         { signed ? <img src={image} className='img-user'/> : ''}
+         { signed ? <Avatar src={`http://localhost:8080/uploads/${image}`} className="img-user">{name.split("")[0]}</Avatar> : ''}
 
-         { signed ? <NavLink to="/profile" className='li'>Profile</NavLink> :  <NavLink to="/login" className='li'>
-            Entrar
-          </NavLink>}
+         { signed ? <NavLink to="/profile" className='li'>Profile</NavLink> :  ""}
 
-         {signed ? <NavLink className='li'>Criar post</NavLink> :  <NavLink to="/register" className='li'>
-          Cadastrar
-          </NavLink>}
+         {signed ? <NavLink to="/create" className='li'>Criar post</NavLink> :  ""}
 
           {signed ? <NavLink onClick={handleLogout} className='logout'>Sair</NavLink> : ''}
 
